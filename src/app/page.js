9e71365064cfdf7459f21a55a5581cde6a7c3dbe
Maycon Vinicius <contato@mayconviniciusdev.com>
2023-10-15 17:38:37 -0300

    Adding small artigo

--- src/app/page.js
+++ src/app/page.js
@@ -1,11 +1,13 @@
 import Header from "@/components/header";
 import Banner from "@/components/banner";
+import Article from "@/components/article";
 
 export default function Home() {
   return (
     <main>
       <Header/>
       <Banner/>
+      <Article/>
     </main>
   )
 }
---
+++ src/components/article.js
@@ -0,0 +1,13 @@
+const Article = () => {
+  return (
+    <div className="max-w-6xl m-auto px-6 font-semibold">
+      <h2 className="text-4xl py-20 text-standard-turquoise/80">Geradores de Senhas Aleatórias: <br/>Sua Aliança na Era Digital?</h2>
+      <p className="py-5 text-xl text-black/60 text-justify leading-relaxed">Você até pode saber quantas senhas já criou ao longo de sua vida. Porém, é inevitável que com tantos serviços online, desde redes sociais a bancos, dizer que não se perdeu em um dado momento entre tantas combinações de letras, números e símbolos. A boa notícia é que você não precisa mais confiar na sua memória para criar senhas seguras. Neste pequeno artigo, irei te apresentar como os geradores de senhas aleatórias funcionam e como poderá criar suas próprias senhas.</p>
+      <p className="py-5 text-xl text-black/60 text-justify leading-relaxed">Explicando sem rodeios, geradores são ferramentas projetada para a criação de senhas complexas de forma extremamente simples. Geralmente se considera o uso de letras maiúsculas, minúsculas, números e símbolos especiais, mas você pode definir o comprimento da senha e as opções de caracteresque deseja e após isso, o trabalho é totalmente do gerador.</p>
+      <p className="py-5 text-xl text-black/60 text-justify leading-relaxed">Senhas geradas aleatoriamente e sem um padrão lógico torna mais difícil o trabalho de hackers e a facilidade de uso dos geradores permite que não seja mais necessário quebrar a cabeça para criar senhas complexas, os mesmos fazem o trabalho para você em segundos. Sua variedade permite infinitas combinações, garantindo senhas exclusivas e seguras.</p>
+      <p className="py-5 text-xl text-black/60 text-justify leading-relaxed">Em resumo, os geradores de senhas são aliados valiosos na proteção de sua presença digital, então não subestime a importância de senhas seguras, pois elas são a primeira linha de defesa contra ameaças. Agora que você entende a sua importância e como os geradores de senhas aleatórias podem ser úteis é hora de começar a utilizá-los e caso tenha interesse em começar a usar algum gerador de senha aleatórias garantindo sua tranquilidade online, <span className="font-semibold text-standard-blue underline underline-offset-8">abaixo você conseguirá criar uma senha conforme seu gosto.</span></p>
+    </div>
+  )
+}
+
+export default Article;
