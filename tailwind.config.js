4f0cf02b99aea9f9d21b91c11858d31d66ec5f42
Maycon Vinicius <contato@mayconviniciusdev.com>
2023-10-15 17:25:12 -0300

    Setting up project

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,9 +8,11 @@ module.exports = {
   theme: {
     extend: {
       backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+        colors: {
+        'standard-blue': '#004369',
+        'standard-turquoise': '#0E7178',
+        'white': '#F6F6F6',
+        'black': '#000000'},
       },
     },
   },
